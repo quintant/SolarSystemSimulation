87116c0c91a442be78cdc05771a9944cfcc05fa0
Sigurjon Ingi <sigurjoni52@gmail.com>
2023-03-14 09:00:12 +0000

    sped

--- spaceManager.py
+++ spaceManager.py
@@ -14,17 +14,17 @@ class SpaceManager:
         self.sun = sun
 
         self.obj_pos = np.random.uniform(-.5, .5, (self.numObjects, 2)) * np.array([[self.width, self.height]]) / 2 + np.array([[self.width, self.height]]) / 2
-        self.obj_pos = torch.tensor(self.obj_pos, dtype=torch.float32, device='cuda', requires_grad=False)
+        self.obj_pos = torch.tensor(self.obj_pos, dtype=torch.float32, device='cpu', requires_grad=False)
 
         self.obj_vel = np.random.randn(self.numObjects, 2) * .01
-        self.obj_vel = torch.tensor(self.obj_vel, dtype=torch.float32, device='cuda', requires_grad=False)
+        self.obj_vel = torch.tensor(self.obj_vel, dtype=torch.float32, device='cpu', requires_grad=False)
 
         self.obj_mass = np.random.randn(self.numObjects) * 100 
-        self.obj_mass = torch.tensor(self.obj_mass, dtype=torch.float32, device='cuda', requires_grad=False)
+        self.obj_mass = torch.tensor(self.obj_mass, dtype=torch.float32, device='cpu', requires_grad=False)
 
-        # self.obj_mass[0] = 10_000
+        self.obj_mass[0] = 10_000
         self.mid = np.array([[self.width, self.height]]) / 2
-        self.mid = torch.tensor(self.mid, dtype=torch.float32, device='cuda', requires_grad=False)
+        self.mid = torch.tensor(self.mid, dtype=torch.float32, device='cpu', requires_grad=False)
 
         self.Z = np.zeros((self.width, self.height, 3), dtype=np.uint8)
         self.surf = pygame.surfarray.make_surface(self.Z)
@@ -38,11 +38,12 @@ class SpaceManager:
             diff = self.obj_pos - self.obj_pos[i]
             dist = torch.sqrt(torch.sum(diff**2, axis=1))
             dist[i] = 1
-            force = (1e-3) * diff * self.obj_mass[i] * self.obj_mass[:, np.newaxis] / dist[:, np.newaxis]**2
-            self.obj_vel[i] += torch.sum(force, axis=0) / self.obj_mass[i]
+            # force = (1e-3) * diff * self.obj_mass[i] * self.obj_mass[:, np.newaxis] / dist[:, np.newaxis]**2
+            force = (1e-3) * diff * self.obj_mass[:, np.newaxis] / dist[:, np.newaxis]**2
+            self.obj_vel[i] += torch.sum(force, axis=0)
         
         self.obj_pos += self.obj_vel
-        # self.obj_pos[0] = self.mid
+        self.obj_pos[0] = self.mid
 
         
         DD = self.obj_pos.to(torch.int32)
